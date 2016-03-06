0f5b374dc5eec84ffa2ed44d2f975fbdb086fa91
John Dong <jdong1992@gmail.com>
2016-03-06 04:01:36 -0800

    initial REST API framework setup

--- food_cart_app/settings.py
+++ food_cart_app/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'login',
+    'rest_framework',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -48,6 +50,11 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'PAGE_SIZE': 10
+}
 
 ROOT_URLCONF = 'food_cart_app.urls'
 
--- food_cart_app/urls.py
+++ food_cart_app/urls.py
@@ -1,10 +1,18 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from rest_framework import routers
+from login import views
 
-urlpatterns = patterns('',
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+urlpatterns = [
     # Examples:
     # url(r'^$', 'food_cart_app.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-)
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
