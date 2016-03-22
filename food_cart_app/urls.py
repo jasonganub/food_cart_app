8c137eee645240a6fafb9a13fa9f7ca201e72d3e
Jason Ganub <jasonganub@gmail.com>
2016-03-21 20:33:55 -0700

    Removed unncessary url and db.sqlite3

--- food_cart_app/urls.py
+++ food_cart_app/urls.py
@@ -14,5 +14,4 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
