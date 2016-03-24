db0ec13a371b2b22cefa4f8c67876f9457eb4715
Jason Ganub <jasonganub@gmail.com>
2016-03-24 13:11:57 -0700

    Created seeker model with create method, needs fixing

--- signup/models.py
+++ signup/models.py
@@ -1,2 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+
+# The fields (first_name, last_name, username, email) will be
+# accesed via the built in User model.
+class Seeker(models.Model):
+    user = models.OneToOneField(User)
+    user.first_name = models.CharField(max_length=30)
+    user.last_name = models.CharField(max_length=30)
+    user.username = models.CharField(max_length=150)
+    user.email = models.EmailField(max_length=100)
+    user.password = models.CharField(max_length=100)
+
+    @classmethod
+    def create(cls, firstname, lastname, username, email, password):
+        new_seeker = cls(
+            first_name=firstname,
+            last_name=lastname,
+            username=username,
+            email=email,
+            password=password)
+        return new_seeker
+
+    def __str__(self):
+        return self.user.username
