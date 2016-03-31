7ba7536b4a3cf75d3709cbea116d0b66555a9986
Jason Ganub <jasonganub@gmail.com>
2016-03-31 08:58:40 -0700

    Added more attributes to the TEMPLATES settings

--- food_cart_app/settings.py
+++ food_cart_app/settings.py
@@ -68,6 +68,15 @@ WSGI_APPLICATION = 'food_cart_app.wsgi.application'
 TEMPLATES = [{
     'BACKEND': 'django.template.backends.django.DjangoTemplates',
     'DIRS': [os.path.join(BASE_DIR, 'templates')],
+    'APP_DIRS': True,
+    'OPTIONS': {
+        'context_processors': [
+            'django.template.context_processors.debug',
+            'django.template.context_processors.request',
+            'django.contrib.auth.context_processors.auth',
+            'django.contrib.messages.context_processors.messages',
+        ],
+    },
 }]
 
 DATABASES = {
