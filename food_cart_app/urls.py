ea7993ce2fe60c07f9964f0cbcc61fc91dfba3bd
Vania Alfitri <alfitriv@gmail.com>
2016-04-13 22:21:14 -0700

    Added login page and url

--- food_cart_app/urls.py
+++ food_cart_app/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', TemplateView.as_view(template_name='landing_page.html')),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^login/', TemplateView.as_view(template_name='login_page.html')),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^', TemplateView.as_view(template_name='landing_page.html'))
 ]
