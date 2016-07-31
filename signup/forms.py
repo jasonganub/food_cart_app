6f23377d2e27e12af0a489816de9600f3920b33b
Jason Ganub <jasonganub@gmail.com>
2016-07-30 18:13:22 -0700

    Added email attribute with validation included.

--- signup/forms.py
+++ signup/forms.py
@@ -15,4 +15,4 @@ class UserModelForm(ModelForm):
 	password = forms.CharField(widget=forms.PasswordInput())
 	class Meta:
 		model = User
-		fields = ['username','first_name','last_name','password']
+		fields = ['username','first_name','last_name','email','password']
