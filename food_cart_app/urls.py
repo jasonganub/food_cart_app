bf51c492e4c2bb934dd240a21f5a750c840167ac
Jason Ganub <jasonganub@gmail.com>
2016-09-29 00:10:29 -0700

    Added logout redirect to home page

--- food_cart_app/urls.py
+++ food_cart_app/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
     url(r'^accounts/register/', include('seeker.urls'), name='signup'),
     url(r'^accounts/login/', 'django.contrib.auth.views.login', name='login'),
+    url(r'^accounts/logout/', 'django.contrib.auth.views.logout',
+                                {'next_page': '/'}, name='logout'),
     url(r'^', TemplateView.as_view(template_name='landing_page.html')),
     url(r'^admin/', include(admin.site.urls)),
 ]
