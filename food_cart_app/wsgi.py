07f7d5a34c460943ac25bda81958cad973a19d9d
Jason Ganub <jasonganub@gmail.com>
2016-09-29 23:42:33 -0700

    Did I really waste 2 days of heroku debugging over this dumb typo?

--- food_cart_app/wsgi.py
+++ food_cart_app/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "foot_cart_app.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "food_cart_app.settings")
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
