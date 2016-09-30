f77367ee350235b1d2982f52474bdc74313b3358
Jason Ganub <jasonganub@gmail.com>
2016-09-29 22:36:02 -0700

    Added project root and static root settings

--- food_cart_app/settings.py
+++ food_cart_app/settings.py
@@ -114,22 +114,19 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 # Login Authentication
 APPEND_SLASH = False
 
-# Heroku
-# Project settings
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Heroku
 # Update database configuration with $DATABASE_URL.
