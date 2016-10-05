28a7ad90df3e33a08d133d8087517560451be393
Jason Ganub <jasonganub@gmail.com>
2016-10-04 19:59:10 -0700

    Created simple unit test for registration page

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Food Cart App
 
+---
 
+Testing
+
+1. run
+
+'python manage.py collectstatic'
+
+2. run
+
+'./manage.py test'
--- seeker/tests.py
+++ seeker/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from seeker.views import register_user
 
-# Create your tests here.
+# Unit Test
+class UserTestCase(TestCase):
+
+	def setUp(self):
+		self.factory = RequestFactory()
+
+	def test_registration_page(self):
+		request = self.factory.get('/accounts/register')
+
+		response = register_user(request)
+		self.assertEqual(response.status_code, 200)
