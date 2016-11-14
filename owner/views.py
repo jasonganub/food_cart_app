9934a106a55862291e8082e1501143bcfebee7b8
Jason Ganub <jasonganub@gmail.com>
2016-11-13 19:57:45 -0800

    Created an html block to display and send trucks

--- owner/views.py
+++ owner/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from .models import Owner
 
 
 def browse_trucks(request):
     if request.method == "GET":
-        pass
+        list_of_trucks = Owner.objects.all()
+        context = {'list_of_trucks': list_of_trucks}
+        return render(request, 'browse.html', context)
     else:
         pass
     return render(request, 'browse.html')
