b70c60caf9d1b69b8354dfb4c703c0e2ef864788
Jason Ganub <jasonganub@gmail.com>
2016-11-13 17:24:58 -0800

    Removed redundant TEMPLATES section

--- food_cart_app/settings.py
+++ food_cart_app/settings.py
@@ -63,20 +63,6 @@ APPEND_SLASH = False
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-TEMPLATES = [{
-    'BACKEND': 'django.template.backends.django.DjangoTemplates',
-    'DIRS': [os.path.join(BASE_DIR, 'templates')],
-    'APP_DIRS': True,
-    'OPTIONS': {
-        'context_processors': [
-            'django.template.context_processors.debug',
-            'django.template.context_processors.request',
-            'django.contrib.auth.context_processors.auth',
-            'django.contrib.messages.context_processors.messages',
-        ],
-    },
-}]
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
