6ad7020b99e1e45143c533d4d5171e76d3aaa04b
Jason Ganub <jasonganub@gmail.com>
2016-11-13 19:09:04 -0800

    Created Owner migration and fixed admin page issue

--- food_cart_app/urls.py
+++ food_cart_app/urls.py
@@ -3,12 +3,14 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 
 
+admin.autodiscover()
+
 urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/register/', include('seeker.urls'), name='signup'),
     url(r'^accounts/login/', 'django.contrib.auth.views.login'),
     url(r'^accounts/logout/', 'django.contrib.auth.views.logout',
                                 {'next_page': '/'}, name='logout'),
     url(r'^browse/', include('owner.urls'), name='browse'),
     url(r'^', TemplateView.as_view(template_name='landing_page.html')),
-    url(r'^admin/', include(admin.site.urls)),
 ]
