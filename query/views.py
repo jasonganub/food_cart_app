a1afcf51a697aa81e2136300f0a26643a9596d82
Jason Ganub <jasonganub@gmail.com>
2017-01-07 21:18:52 -0800

    Created initial methods for views

--- query/views.py
+++ query/views.py
@@ -1,2 +1,15 @@
 from django.shortcuts import render
 from openinghours.models import Company, Openinghours
+
+#TODO: Create function to search and view detailed company
+def view_detailed_company(request):
+    if request.method == "GET":
+        pass
+
+def display_all_companies(request):
+    if request.method == "GET":
+        pass
+
+def display_company_by_id(request):
+    if request,method == "GET":
+        pass
